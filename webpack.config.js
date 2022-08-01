4abdaaa5003e18def7b0a955a06967baa592ddee
Syknapse <29199184+Syknapse@users.noreply.github.com>
2022-08-01 16:40:35 +0200

    Set up webpack development workflow
    Downgrade to earlier versions of installed packages for compatibility with webpack-dev-server used in the tutorial

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,14 @@
 const path = require('path')
 
 module.exports = {
+  mode: 'development',
   entry: './src/app.ts',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    publicPath: 'dist' /* Needed so webpack-dev-server knows to look in dist and save the bundle to memory */,
   },
-  devtool: 'inline-source-map',
+  devtool: 'inline-source-map' /* Tells webpack we have generated source maps (from tsconfig) to show in devtools */,
   module: {
     rules: [
       {
